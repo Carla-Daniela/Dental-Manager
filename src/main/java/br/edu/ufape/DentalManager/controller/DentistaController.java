83afa7c849db265cac80bc063558be3d8da8a85c
milena <milenaferreiradacostasantos@gmail.com>
2023-04-19 22:02:27 -0300

    teste de api

--- src/main/java/br/edu/ufape/DentalManager/controller/DentistaController.java
+++ src/main/java/br/edu/ufape/DentalManager/controller/DentistaController.java
@@ -1,5 +1,26 @@
 package br.edu.ufape.DentalManager.controller;
 
-public class DentistaController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import br.edu.ufape.DentalManager.negocio.basica.Agenda;
+import br.edu.ufape.DentalManager.negocio.basica.Agendamento;
+import br.edu.ufape.DentalManager.negocio.fachada.Dentalmanager;
+
+@CrossOrigin (origins = "http://localhost:8081/" )
+@RestController
+public class DentistaController {
+	
+	@Autowired
+	private Dentalmanager gerenciador;
+	
+	@PostMapping(value="/Dentista/Agendar")
+	public ResponseEntity<String>CriarAgenda(@RequestBody Agenda agenda){
+		gerenciador.CriarAgenda(agenda);
+		return ResponseEntity.ok("Agenda criada");
+	}
 }
